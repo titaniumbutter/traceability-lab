7118e6cb30922d9cc640f67d7b68c3aee423c629
titaniumbutter <israelfranco1998@gmail.com>
2021-06-29 15:18:34 -0500

    added list

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var rollbar = new Rollbar({
 });
 
 app.use(express.json())
+app.use('/style', express.static('./public/styles.css'))
+
+let students = []
 
 app.get('/', function(req, res) {
 
@@ -17,6 +20,35 @@ app.get('/', function(req, res) {
     rollbar.critical('Crash while trying to load the page')
     rollbar.warning('Webpage API unavailable')
     res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.post('/api/student', (req, res) => {
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex((studentName) => { // check if student name exists already
+        return studentName === name
+    })
+
+console.log(index)
+
+try { // using a "try catch" block will handle any generic 500 errors (not necessary, but a good addition)
+    if (index === -1 && name !== '') {
+        // we'll send responses to the user based upon whether or not they gave us a valid user to add
+        // also we'll send information to rollbar so we can keep track of the activity that's happening
+        students.push(name)
+        rollbar.log('student added successfully', {author: 'riley', type: 'manual'})
+        res.status(200).send(students)
+    } else if (name === '') {
+        rollbar.error('no name given')
+        res.status(400).send('must provide a name')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+} catch (err) {
+    rollbar.error(err)
+}
 })
 
 
